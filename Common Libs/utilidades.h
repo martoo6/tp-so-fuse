1e06775fa98e441a5d66436cdc267bc491def1f7
martoo6 <martoo6@gmail.com>
2011-10-24 21:44:40 +0000

    Funciones any_satisfy, all_satisfy, map, y filter de Haskell llevadas a C.

--- Common Libs/utilidades.h
+++ Common Libs/utilidades.h
@@ -11,7 +11,10 @@
 #include <semaphore.h>
 #include "../C-Talks Libs/linux/common-libs/src/lib/collections/list.h"
 
-char any_satisfy( t_list* list, char (*closure)(void*, void*),void *arg);
-char all_satisfy( t_list* list, char (*closure)(void*, void*),void *arg);
+char collection_any_satisfy( t_list* list, char (*closure)(void*, void*),void *arg);
+char collection_all_satisfy( t_list* list, char (*closure)(void*, void*),void *arg);
+void *collection_filter( t_list* list, char (*closure)(void*, void*),void *arg);
+void *collection_map( t_list* list, void *(*closure)(void*, void*),void *arg);
+
 
 #endif /* UTILIDADES_H_ */

c79f11e13e58c64d16770d86ad032c72d1a02463
martoo6@gmail.com <martoo6@gmail.com@c7e5351b-48aa-88fe-af7c-d8807a2c5520>
2011-10-24 21:44:40 +0000

    Funciones any_satisfy, all_satisfy, map, y filter de Haskell llevadas a C.

--- trunk/Common Libs/utilidades.c
+++ trunk/Common Libs/utilidades.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <semaphore.h>
 #include "utilidades.h"
+//#include "../PPD/PPD.h"
 
 /*
- * @NAME: collection_list_iterator
- * @DESC: Itera la lista llamando al closure por cada elemento
+ * @NAME: collection_any_satisfy
+ * @DESC: Igual a funcion any_satisfy de Haskell
  */
-char any_satisfy( t_list* list, char (*closure)(void*, void*),void *arg){
+char collection_any_satisfy( t_list* list, char (*closure)(void*, void*),void *arg){
 	t_link_element *element = list->head;
 	sem_wait( &list->semaforo );
 	while( element != NULL ){
@@ -18,8 +19,11 @@ char any_satisfy( t_list* list, char (*closure)(void*, void*),void *arg){
 	sem_post( &list->semaforo );
 	return(0);
 }
-
-char all_satisfy( t_list* list, char (*closure)(void*, void*),void *arg){
+/*
+ * @NAME: collection_all_satisfy
+ * @DESC: Igual a funcion all_satisfy de Haskell
+ */
+char collection_all_satisfy( t_list* list, char (*closure)(void*, void*),void *arg){
 	t_link_element *element = list->head;
 	sem_wait( &list->semaforo );
 	while( element != NULL ){
@@ -30,4 +34,40 @@ char all_satisfy( t_list* list, char (*closure)(void*, void*),void *arg){
 	}
 	sem_post( &list->semaforo );
 	return(1);
+}
+/*
+ * @NAME: collection_filter
+ * @DESC: Igual a la funcion filter de haskell
+ */
+void *collection_filter( t_list* list, char (*closure)(void*, void*),void *arg){
+	t_list *listaAux=collection_list_create();
+	t_link_element *element = list->head;
+	sem_wait( &list->semaforo );
+	while( element != NULL ){
+		if(closure(element->data, arg)){
+			void *newData=malloc(sizeof(element->data));
+			newData=element->data;
+			collection_list_add(listaAux,newData);
+		}
+		element = element->next;
+	}
+	sem_post( &list->semaforo );
+	return(listaAux);
+}
+/*
+ * @NAME: collection_map
+ * @DESC: Igual a la funcion map de haskell
+ */
+void *collection_map( t_list* list, void *(*closure)(void*, void*),void *arg){
+	t_list *listaAux=collection_list_create();
+	t_link_element *element = list->head;
+	sem_wait( &list->semaforo );
+	while( element != NULL ){
+		void *newData=malloc(sizeof(*closure));
+		newData=closure(element->data, arg);
+		collection_list_add(listaAux,newData);
+		element = element->next;
+	}
+	sem_post( &list->semaforo );
+	return(listaAux);
 }

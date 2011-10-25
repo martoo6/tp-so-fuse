9df1122fb1926d0f98774b952f369fcad702293f
martoo6@gmail.com <martoo6@gmail.com@c7e5351b-48aa-88fe-af7c-d8807a2c5520>
2011-10-25 03:29:14 +0000

    Funciones de orden superior con argumentos variables.

--- trunk/Common Libs/utilidades.h
+++ trunk/Common Libs/utilidades.h
@@ -9,12 +9,17 @@
 #define UTILIDADES_H_
 
 #include <semaphore.h>
+#include <stdarg.h> //PARA ARGUMENTOS VARIABLES YAY !
 #include "../C-Talks Libs/linux/common-libs/src/lib/collections/list.h"
 
 char collection_any_satisfy( t_list* list, char (*closure)(void*, void*),void *arg);
+char collection_any_satisfy2( t_list* list, char (*closure)(void*, ...),...);
 char collection_all_satisfy( t_list* list, char (*closure)(void*, void*),void *arg);
+char collection_all_satisfy2( t_list* list, char (*closure)(void*, ...),...);
 void *collection_filter( t_list* list, char (*closure)(void*, void*),void *arg);
+void *collection_filter2( t_list* list, char (*closure)(void*, ...),...);
 void *collection_map( t_list* list, void *(*closure)(void*, void*),void *arg);
+void *collection_map2( t_list* list, void *(*closure)(void*, ...),...);
 
 
 #endif /* UTILIDADES_H_ */

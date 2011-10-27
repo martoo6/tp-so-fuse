ecbf723343404007944f29d88c6cf90f5fe13b07
martoo6@gmail.com <martoo6@gmail.com@c7e5351b-48aa-88fe-af7c-d8807a2c5520>
2011-10-27 00:18:53 +0000

    Prueba de funcionamiento PPD-Consola existosa. Faltan Sockets UNIX, y parsea de comandos.

---
+++ trunk/PPD-Consola/PPD-Consola.c
@@ -0,0 +1,12 @@
+/*
+ * PPD-Consola.c
+ *
+ *  Created on: 26/10/2011
+ *      Author: Martin Silberkasten
+ */
+#include <stdio.h>
+
+int main(int argc,char *argv[]){
+	printf("Que haces papa ?, soy un super duper proceso cargado en memoria !");
+	return 0;
+}
---
+++ trunk/PPD-Consola/PPD-Consola.h
@@ -0,0 +1,13 @@
+/*
+ * PPD-Consola.h
+ *
+ *  Created on: 26/10/2011
+ *      Author: Martin Silberkasten
+ */
+
+#ifndef PPD_CONSOLA_H_
+#define PPD_CONSOLA_H_
+
+
+
+#endif /* PPD_CONSOLA_H_ */

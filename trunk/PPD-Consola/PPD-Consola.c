2c57047d3422d49ad2c12fe906473a7577fc043b
martoo6@gmail.com <martoo6@gmail.com@c7e5351b-48aa-88fe-af7c-d8807a2c5520>
2011-10-29 12:28:55 +0000

    Actualizacion para llevar todo a medrano.

--- trunk/PPD-Consola/PPD-Consola.c
+++ trunk/PPD-Consola/PPD-Consola.c
@@ -4,9 +4,48 @@
  *  Created on: 26/10/2011
  *      Author: Martin Silberkasten
  */
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <pthread.h>
+#include <stdarg.h>
+
+#include "list.c"
+#include "array.c"
+#include "scanner.c"
+#include "commands.c"
+
+void funcInfo(void *nada, t_array *nada2){
+	printf("Posicion actual: \n");
+}
+void funcClean(void *nada, t_array *clusters){
+	//NO FUNCIONA ARRAY_GET y la "!#$%$&/%$/
+	int *clusterInicial, *clusterFinal;
+	clusterInicial = array_get(clusters,1);
+	clusterFinal = array_get(clusters,2);
+	printf("Cluster inicial: %d\n",clusters->data[0]);
+	printf("Cluster final: %d\n",clusters->data[1]);
+}
+void funcTrace(void *nada, t_array *nada2){
+	printf("Posicion actual:");
+}
 
 int main(int argc,char *argv[]){
-	printf("Que haces papa ?, soy un super duper proceso cargado en memoria !");
+	t_commands *comandos = commands_create("-", "", " ");
+	commands_add(comandos, "info",funcInfo);
+	commands_add(comandos, "clean",funcClean);
+	commands_add(comandos, "trace",funcTrace);
+
+	char input[20];
+	while(1){
+		scanf("%s",input);
+		commands_parser(comandos,input);
+		sleep(1);
+	}
 	return 0;
 }
